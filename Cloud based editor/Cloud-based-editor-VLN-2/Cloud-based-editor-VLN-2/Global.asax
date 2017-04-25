177525abd903cedaccc7f38b2c808c4cdaa608a7
Daníel Melstað <danielm15@ru.is>
2017-04-25 09:56:24 +0000

    Project skeleton

---
+++ Cloud based editor/Cloud-based-editor-VLN-2/Cloud-based-editor-VLN-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cloud_based_editor_VLN_2.MvcApplication" Language="C#" %>
---
+++ Cloud based editor/Cloud-based-editor-VLN-2/Cloud-based-editor-VLN-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cloud_based_editor_VLN_2.Startup))]
+namespace Cloud_based_editor_VLN_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

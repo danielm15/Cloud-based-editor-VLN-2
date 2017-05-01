b84e15fd68f9b6f703cf1e7ae66c24502d351597
Daníel Melstað <danielm15@ru.is>
2017-05-01 16:46:21 +0000

    Added Viewmodel classes and some basic structure to ProjectService.cs

---
+++ Cloud-based-editor-VLN-2/Models/ViewModels/ProjectFileViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloud_based_editor_VLN_2.Models.ViewModels {
+    public class ProjectFileViewModel {
+    }
+}
--- Cloud-based-editor-VLN-2/Services/ProjectService.cs
+++ Cloud-based-editor-VLN-2/Services/ProjectService.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using Cloud_based_editor_VLN_2.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Cloud_based_editor_VLN_2.Services {
     public class ProjectService {
+
+        // Fetch all projects owned by a particular User
+        public List<ProjectViewModel> GetProjectsByUserID(int UserID) {
+            // TODO
+            return null;
+        }
+
+        // Fetch a single project by ProjectID
+        public ProjectViewModel GetProjectByID(int ProjectID) {
+            // TODO
+            return null;
+        }
+
+        // Fetch all files in a single project
+        public List<ProjectFileViewModel> GetProjectFilesByID(int ProjectID) {
+            // TODO
+            return null;
+        }
     }
+
 }

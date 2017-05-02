85f9073e723ad0d48e3aa6fdbc49a613b8289435
Daníel Melstað <danielm15@ru.is>
2017-05-02 11:56:55 +0000

    Changed database to Code-First

--- Cloud-based-editor-VLN-2/Models/Entities/Project.cs
+++ Cloud-based-editor-VLN-2/Models/Entities/Project.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,22 @@ namespace Cloud_based_editor_VLN_2.Models.Entities {
     public class Project {
         [Key]
         public int ID { get; set; }
-        public int ownerID { get; set; }
-        public string Name { get; set; }
-        public DateTime DateCreated { get; set; }
-        public int StartUpFileID { get; set; }
 
-        public virtual List<Document> ProjectFiles { get; set; }
+        public int OwnerID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int? StartUpFileID { get; set; }
+
+        public virtual List<Document> Documents { get; set; }
+
+        [ForeignKey("OwnerID")]
+        public virtual AppUser AppUser { get; set; }
+
+        [ForeignKey("StartUpFileID")]
+        public virtual Document Document { get; set; }
         
     }
 }

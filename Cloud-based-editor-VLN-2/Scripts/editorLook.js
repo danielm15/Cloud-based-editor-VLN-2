f061a1212d68b10360091540c1b1ca5f0ad0b429
bensi94 <bensi94@icloud.com>
2017-05-03 23:12:23 +0000

    navigationMenu

--- Cloud-based-editor-VLN-2/Scripts/editorLook.js
+++ Cloud-based-editor-VLN-2/Scripts/editorLook.js
@@ -14,8 +14,29 @@ function showNavBar() {
     document.getElementById("sideNav").classList.toggle("activeNav");
     if (document.getElementById("editorContentId").style.marginLeft == "200px") {
         document.getElementById("editorContentId").style.marginLeft = "0px";
+        document.getElementById("verticalTextID").style.borderBottom = "1px solid darkgray";
     } else {
         document.getElementById("editorContentId").style.marginLeft = "200px";
+        document.getElementById("verticalTextID").style.borderBottom = "1px solid white";
     }
     
+}
+
+function showHeader(id) {
+    var classElementsNav = document.getElementsByClassName("navigationLink");
+
+    for (var i = 0; i < classElementsNav.length; i++) {
+        var element = classElementsNav[i];
+        element.classList.remove("selectedNav");
+    }
+    document.getElementById(id).classList.toggle("selectedNav");
+
+    var id = "doc" + id.substring(3, 4);
+    var classElements = document.getElementsByClassName("hideHeader");
+    
+    for (var i = 0; i < classElements.length; i++) {
+        var element = classElements[i];
+        element.classList.remove("showHeader");
+    }
+    document.getElementById(id).classList.toggle("showHeader");
 }

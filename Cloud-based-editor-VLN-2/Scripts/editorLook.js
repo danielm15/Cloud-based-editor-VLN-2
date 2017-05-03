e4f79ab681c45518e28e7093ef173ef463a59cd0
bensi94 <bensi94@icloud.com>
2017-05-03 14:30:54 +0000

    navigation

--- Cloud-based-editor-VLN-2/Scripts/editorLook.js
+++ Cloud-based-editor-VLN-2/Scripts/editorLook.js
@@ -1,5 +1,20 @@
 ﻿function bodyMargin() {
     document.getElementById("bodyId").classList.toggle("addToBody");
-    document.getElementById("containerBodyId").classList.toggle("changeWidth"); 
+    document.getElementById("containerBodyId").classList.toggle("changeWidth");
+    document.getElementById("containerHeaderId").classList.toggle("changeWidth");
+    
+}
 
+function hideFooter() {
+    document.getElementById("footerId").classList.toggle("hideFooter");
+}
+
+function showNavBar() {
+    document.getElementById("sideNav").classList.toggle("activeNav");
+    if (document.getElementById("editorContentId").style.marginLeft == "200px") {
+        document.getElementById("editorContentId").style.marginLeft = "0px";
+    } else {
+        document.getElementById("editorContentId").style.marginLeft = "200px";
+    }
+    
 }

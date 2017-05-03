0b242c02709d899180d525eae44adec0b296c9da
sbaldurs <solvib11@ru.is>
2017-05-03 14:16:24 +0000

    You can now add a document to your project

--- Cloud-based-editor-VLN-2/Models/ViewModels/DocumentViewModel.cs
+++ Cloud-based-editor-VLN-2/Models/ViewModels/DocumentViewModel.cs
@@ -6,6 +6,8 @@ using System.Web;
 
 namespace Cloud_based_editor_VLN_2.Models.ViewModels {
     public class DocumentViewModel {
+        public int CurrProjectID { get; set; }
+        public Document Doc { get; set; }
         public IEnumerable<Document> Documents { get; set; }
     }
 }

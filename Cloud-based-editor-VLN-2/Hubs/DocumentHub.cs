94cef4436964f2258828ca34e822664cb7d8cd23
Daníel Melstað <danielm15@ru.is>
2017-05-04 13:39:40 +0000

    Added SignalR to project and Hub class, aswell as LastUpdatedBy member variable to document

---
+++ Cloud-based-editor-VLN-2/Hubs/DocumentHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Cloud_based_editor_VLN_2.Models.Entities;
+
+namespace Cloud_based_editor_VLN_2.Hubs {
+    public class DocumentHub : Hub {
+
+        public void UpdateDocument(Document documentModel) {
+            documentModel.LastUpdatedBy = Context.ConnectionId;
+            Clients.AllExcept(documentModel.LastUpdatedBy).updateText(documentModel);
+        }
+    }
+}

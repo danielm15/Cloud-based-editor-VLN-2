6ba415aa088b75484b6c8ef10abf1bb5fe60bd79
bensi94 <bensi94@icloud.com>
2017-05-05 17:46:21 +0000

    CreateFile

--- Cloud-based-editor-VLN-2/Scripts/documentList.js
+++ Cloud-based-editor-VLN-2/Scripts/documentList.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(function () {
+    $("#createFileForm").on("submit", function () {
+        var form = $(this);
+        $.ajax({
+            method: "post",
+            url: "Document/Create",
+            data: form.serialize(),
+            success: function (respnse) {
+
+            }
+        });
+        return false;
+    });
+});

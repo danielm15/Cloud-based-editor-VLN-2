5f0409954df63c2655b935a61147c18aa32d5a51
Daníel Melstað <danielm15@ru.is>
2017-05-10 16:56:07 +0000

    Unit tests for AppUserService

--- Cloud-based-editor-VLN-2.Tests/Services/AppUserServiceTest.cs
+++ Cloud-based-editor-VLN-2.Tests/Services/AppUserServiceTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Cloud_based_editor_VLN_2.Services;
 using Cloud_based_editor_VLN_2.Models.Entities;
 using Cloud_based_editor_VLN_2.Tests;
+using System.Collections.Generic;
 
 namespace Cloud_based_editor_VLN_2.Tests.Services {
     [TestClass]
@@ -10,9 +11,15 @@ namespace Cloud_based_editor_VLN_2.Tests.Services {
 
         private AppUserService _AppUserService;
 
-        [TestMethod]
-        public void TestAddUser() {
-            // Arrange:
+        static T First<T>(IEnumerable<T> items) {
+            using (IEnumerator<T> iter = items.GetEnumerator()) {
+                iter.MoveNext();
+                return iter.Current;
+            }
+        }
+
+        [TestInitialize]
+        public void Initialize() {
             var mockContext = new MockDataContext();
 
             var user1 = new AppUser {
@@ -20,30 +27,146 @@ namespace Cloud_based_editor_VLN_2.Tests.Services {
                 UserName = "User1",
                 Email = "Email1@Email1.com"
             };
+            mockContext.AppUsers.Add(user1);
 
             var user2 = new AppUser {
                 ID = 2,
                 UserName = "User2",
                 Email = "Email2@Email2.com"
             };
+            mockContext.AppUsers.Add(user2);
 
             var user3 = new AppUser {
                 ID = 3,
                 UserName = "User3",
                 Email = "Email3@Email3.com"
             };
-
-            // Act:
-            mockContext.AppUsers.Add(user1);
-            mockContext.AppUsers.Add(user2);
             mockContext.AppUsers.Add(user3);
-            _AppUserService = new AppUserService(mockContext);
-            
-            //CollectionAssert.
 
+            var project1 = new Project {
+                ID = 1,
+                OwnerID = 1,
+                Name = "Project1",
+                DateCreated = new DateTime(2017, 1, 1),
+                AppUser = user1,
+                ProjectType = "HTML"
+            };
+
+            var userProject1 = new UserProjects {
+                ID = 1,
+                AppUserID = 1,
+                ProjectID = 1,
+                AppUser = user1,
+                Project = project1
+            };
+
+            mockContext.UserProjects.Add(userProject1);
+
+            _AppUserService = new AppUserService(mockContext);
+        }
+
+        [TestMethod]
+        public void TestAddUser() {
+            // Arrange:
+            var user4 = new AppUser {
+                ID = 4,
+                UserName = "User4",
+                Email = "Email4@Email4.com"
+            };
+            // Act:
+            _AppUserService.addUser(user4);
+
+            var AllUsers = _AppUserService.getAllUsers();
 
             // Assert:
-            
+            Assert.AreEqual(4, AllUsers.Count);
+            Assert.AreEqual(4, AllUsers[3].ID);
+            Assert.AreEqual("User4", AllUsers[3].UserName);
+            Assert.AreEqual("Email4@Email4.com", AllUsers[3].Email);
+        }
+
+        [TestMethod]
+        public void TestAddUserToProject() {
+            // Arrange:
+            var user2 = new AppUser {
+                ID = 2,
+                UserName = "User2",
+                Email = "Email2@Email2.com"
+            };
+
+            var project2 = new Project {
+                ID = 2,
+                OwnerID = 2,
+                Name = "Project2",
+                DateCreated = new DateTime(2017, 1, 1),
+                AppUser = user2,
+                ProjectType = "HTML"
+            };
+
+            var userProject2 = new UserProjects {
+                ID = 2,
+                AppUserID = 2,
+                ProjectID = 2,
+                AppUser = user2,
+                Project = project2
+            };
+            // Act:
+            _AppUserService.addUserToProject(userProject2);
+            var AllUserProjects = _AppUserService.getAllUserProjects();
+
+            // Assert:
+            Assert.AreEqual(2, AllUserProjects.Count);
+            Assert.AreEqual(2, AllUserProjects[1].ID);
+            Assert.AreEqual(2, AllUserProjects[1].AppUserID);
+            Assert.AreEqual(user2, AllUserProjects[1].AppUser);
+            Assert.AreEqual(project2, AllUserProjects[1].Project);
+        }
+
+        [TestMethod]
+        public void GetAllUsers() {
+            // Arrange:
+
+            // Act:
+            var AllUsers = _AppUserService.getAllUsers();
+
+            // Assert:
+            Assert.IsNotNull(AllUsers);
+            Assert.AreEqual(3, AllUsers.Count);
+            Assert.AreNotEqual(4, AllUsers.Count);
+            Assert.AreEqual(1, AllUsers[0].ID);
+        }
+
+        [TestMethod]
+        public void GetLimitedUserList() {
+            // Arrange:
+            // Act:
+            IEnumerable<AppUser> FoundUsers = _AppUserService.getLimitedUserList("User1");
+
+            // Assert:
+            Assert.IsNotNull(FoundUsers);
+            Assert.AreEqual(1, First<AppUser>(FoundUsers).ID);
+            Assert.AreEqual("User1", First<AppUser>(FoundUsers).UserName);
+            Assert.AreEqual("Email1@Email1.com", First<AppUser>(FoundUsers).Email);
+        }
+
+        [TestMethod]
+        public void GetAllUserProjects() {
+            // Arrange:
+            var user1 = new AppUser {
+                ID = 1,
+                UserName = "User1",
+                Email = "Email1@Email1.com"
+            };
+            // Act:
+            var AllUserProjects = _AppUserService.getAllUserProjects();
+
+            // Assert:
+            Assert.IsNotNull(AllUserProjects);
+            Assert.AreEqual(1, AllUserProjects.Count);
+            Assert.AreEqual(1, AllUserProjects[0].ID);
+            Assert.AreEqual(1, AllUserProjects[0].AppUserID);
+            Assert.AreEqual(1, AllUserProjects[0].ProjectID);
+            Assert.AreEqual("User1", AllUserProjects[0].AppUser.UserName);
         }
     }
 }

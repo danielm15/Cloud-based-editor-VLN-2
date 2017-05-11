d78c177263496d3e78a421f230a3d64fd4c11e9b
Daníel Melstað <danielm15@ru.is>
2017-05-11 20:57:47 +0000

    Updated Document.Name to be always non-null

--- Cloud-based-editor-VLN-2/Migrations/201705091515214_NameProject.cs
+++ Cloud-based-editor-VLN-2/Migrations/201705091515214_NameProject.cs
@@ -7,7 +7,7 @@ namespace Cloud_based_editor_VLN_2.Migrations
         public override void Up()
         {
             DropIndex("dbo.Documents", new[] { "ProjectID" });
-            AlterColumn("dbo.Documents", "Name", c => c.String(maxLength: 400));
+            AlterColumn("dbo.Documents", "Name", c => c.String(maxLength: 400, nullable: false));
             AlterColumn("dbo.Projects", "Name", c => c.String(nullable: false));
             CreateIndex("dbo.Documents", new[] { "Name", "ProjectID" }, unique: true, name: "NameProject");
         }

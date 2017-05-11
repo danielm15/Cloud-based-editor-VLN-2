190c0d2ce18c1238731ce38a74069bb0b316e125
bensi94 <bensi94@icloud.com>
2017-05-11 22:45:56 +0000

    someChanges

--- Cloud-based-editor-VLN-2/Scripts/ProjectOverview.js
+++ Cloud-based-editor-VLN-2/Scripts/ProjectOverview.js
@@ -304,8 +304,8 @@ var makeAdminOfProject = function (projectID, UserID) {
         type: "POST",
         url: "/Project/MakeAdmin",
         data: { id: projectID, userID: UserID },
-        sucess: function (response) {
-          
+        success: function (response) {
+            listCollaboratorsFunc(projectID);
         }
     })
 }
